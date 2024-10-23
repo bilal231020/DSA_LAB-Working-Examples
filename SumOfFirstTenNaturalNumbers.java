1a432eec8e3114278190585e98feb78d6f381b70
bilal231020 <bilalqaiser4210@icloud.com>
2024-10-22 21:39:31 -0700

    Add files via upload

---
+++ SumOfFirstTenNaturalNumbers.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sumoffirsttennaturalnumbers;
+
+/**
+ *
+ * @author 123
+ */
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class SumOfFirstTenNaturalNumbers {
+    public static void main(String[] args) {
+        int sum =0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i; 
+        }
+
+        System.out.println("The sum of the first 10 natural numbers is: " + sum);
+    }
+}
+        
+    
